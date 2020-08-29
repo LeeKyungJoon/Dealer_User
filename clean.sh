8343861933eb3737b171248551c19a077006478e
lee <lyhdra99@svcorps.dev>
2020-08-29 23:45:09 +0900

    init

--- clean.sh
+++ clean.sh
@@ -1,7 +1,8 @@
 echo '*****************************************'
 echo 'Remove node_modules folder'
 echo '*****************************************'
-rm -rf ./node_modules
+rm -rf ./node_modules yarn.lock
+yarn cache clean
 echo '*****************************************'
 echo 'Yarn install'
 echo '*****************************************'
@@ -16,8 +17,19 @@ npx pod-install
 echo '*****************************************'
 echo 'Android Clean Start'
 echo '*****************************************'
+
+N=$PWD
+cd ~
+cd Library/Android/sdk
+A="sdk.dir="
+A+=$PWD
+cd $N
+echo $A > ./android/local.properties
+
 rm -rf ./android/.idea
 rm -rf ./android/build
 rm -rf ./android/.gradle
 cd ./android && ./gradlew clean && cd ..
+open ./ios/*.xcworkspace
+open -a /Applications/Android\ Studio.app ./android/
 echo 'Finished'
