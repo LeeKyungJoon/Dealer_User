830c86ed766c03087a0cf5ec789d61e8cf5cf503
이경준 <wns9654@svcorps.dev>
2020-09-08 17:58:30 +0900

    ***이경준 검색화면 완료

--- src/screens/Tabs/MyCarBuy/SearchCar.js
+++ src/screens/Tabs/MyCarBuy/SearchCar.js
@@ -10,11 +10,28 @@ import {
   SafeAreaView,
   Text,
   View,
+  FlatList,
 } from "react-native";
 
 const Width = Dimensions.get("window").width;
 
 export default function SearchCar() {
+  const [flag, setFlag] = useState(true);
+  const [brandList, setBrandList] = useState([
+    { image: require("../../../images/hyundai.png"), name: "현대" },
+    { image: require("../../../images/jenesis.png"), name: "제네시스" },
+    { image: require("../../../images/kia.png"), name: "기아" },
+    { image: require("../../../images/shevore.png"), name: "쉐보레" },
+  ]);
+
+  const [recently, setRecently] = useState([
+    { name: "포르쉐 뉴 카이엔" },
+    { name: "포드 머스탱" },
+    { name: "현대 싼타페TM" },
+    { name: "포르쉐 파나메라" },
+  ]);
+  const [focuslist, setFocusList] = useState(["최근 검색", "즐겨찾기"]);
+  const [focusSelect, setFocusSelect] = useState("최근 검색");
   const [toblist, setTopList] = useState([
     "국산차",
     "수입차",
@@ -22,6 +39,77 @@ export default function SearchCar() {
     "화물특장버스",
   ]);
   const [select, setSelect] = useState("국산차");
+
+  const _renderItem = (item) => {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: scale(15),
+          borderBottomColor: "#e8e8e8",
+          borderBottomWidth: 1,
+          paddingVertical: scale(15),
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Image style={{ ...styles.brandimage }} source={item.item.image} />
+          <Text style={{ ...styles.brandtext, marginLeft: scale(15) }}>
+            {item.item.name}
+          </Text>
+        </View>
+        <Text style={{ ...styles.count }}>27,954</Text>
+      </View>
+    );
+  };
+
+  const _renderItem1 = (item) => {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: scale(15),
+          borderBottomColor: "#e8e8e8",
+          borderBottomWidth: 1,
+          paddingVertical: scale(10),
+        }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <TouchableOpacity delayPressIn={0}>
+            <Image
+              style={{ ...styles.likes }}
+              source={require("../../../images/likes_on.png")}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity delayPressIn={0}>
+            <Text
+              style={{
+                ...styles.recentlytext,
+                marginLeft: scale(10),
+                marginTop: scale(2),
+              }}
+            >
+              {item.item.name}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity delayPressIn={0}>
+          <Image
+            style={{ ...styles.delete }}
+            source={require("../../../images/delete_ic_60.png")}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <>
@@ -51,6 +139,12 @@ export default function SearchCar() {
             style={{ ...styles.headerinput }}
             placeholder={"브랜드, 차종명 검색"}
             placeholderTextColor="#dedede"
+            onFocus={() => {
+              setFlag(false);
+            }}
+            onBlur={() => {
+              setFlag(true);
+            }}
           />
         }
         rightComponent={
@@ -63,58 +157,129 @@ export default function SearchCar() {
         }
       />
       <SafeAreaView style={{ ...styles.container }}>
-        <View
-          style={{
-            ...styles.tobtab,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingHorizontal: scale(15),
-            alignItems: "center",
-          }}
-        >
-          {toblist.map((item, index) => {
-            return (
-              <View
-                key={index + Math.random()}
-                style={{ alignItems: "center" }}
-              >
-                <TouchableOpacity
-                  delayPressIn={0}
-                  onPress={() => {
-                    setSelect(item);
-                  }}
+        {flag ? (
+          <View
+            style={{
+              ...styles.tobtab,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingHorizontal: scale(15),
+              alignItems: "center",
+            }}
+          >
+            {toblist.map((item, index) => {
+              return (
+                <View
+                  key={index + Math.random()}
+                  style={{ alignItems: "center" }}
                 >
-                  <Text
-                    style={{
-                      ...styles.toptabtext,
-                      opacity: select === item ? 1 : 0.6,
-                      marginBottom: scale(10),
+                  <TouchableOpacity
+                    delayPressIn={0}
+                    onPress={() => {
+                      setSelect(item);
                     }}
                   >
-                    {item}
-                  </Text>
-                </TouchableOpacity>
-                {select === item ? (
-                  <View
-                    style={{
-                      ...styles.toptabsmallbox,
-                      borderTopColor: "#459bfe",
-                      marginBottom: -scale(20),
+                    <Text
+                      style={{
+                        ...styles.toptabtext,
+                        opacity: select === item ? 1 : 0.6,
+                        marginBottom: scale(10),
+                      }}
+                    >
+                      {item}
+                    </Text>
+                  </TouchableOpacity>
+                  {select === item ? (
+                    <View
+                      style={{
+                        ...styles.toptabsmallbox,
+                        borderTopColor: "#459bfe",
+                        marginBottom: -scale(20),
+                      }}
+                    />
+                  ) : (
+                    <View
+                      style={{
+                        ...styles.toptabsmallbox,
+                        borderTopColor: "transparent",
+                        marginBottom: -scale(20),
+                      }}
+                    />
+                  )}
+                </View>
+              );
+            })}
+          </View>
+        ) : (
+          <View
+            style={{
+              ...styles.focustab,
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: scale(15),
+            }}
+          >
+            {focuslist.map((item, index) => {
+              return (
+                <View
+                  key={index + Math.random()}
+                  style={{ alignItems: "center", marginRight: scale(40) }}
+                >
+                  <TouchableOpacity
+                    onPress={() => {
+                      setFocusSelect(item);
                     }}
-                  />
-                ) : (
-                  <View
-                    style={{
-                      ...styles.toptabsmallbox,
-                      borderTopColor: "transparent",
-                      marginBottom: -scale(20),
-                    }}
-                  />
-                )}
-              </View>
-            );
-          })}
-        </View>
+                    delayPressIn={0}
+                  >
+                    <Text
+                      style={{
+                        ...styles.focustabtext,
+                        color: focusSelect === item ? "#222222" : "#b2b2b2",
+                        marginBottom: scale(10),
+                      }}
+                    >
+                      {item}
+                    </Text>
+                  </TouchableOpacity>
+                  {focusSelect === item ? (
+                    <View
+                      style={{
+                        ...styles.focustabsmallbox,
+                        borderTopColor: "#f3f3f3",
+                        marginBottom: -scale(20),
+                      }}
+                    />
+                  ) : (
+                    <View
+                      style={{
+                        ...styles.focustabsmallbox,
+                        borderTopColor: "transparent",
+                        marginBottom: -scale(20),
+                      }}
+                    />
+                  )}
+                </View>
+              );
+            })}
+          </View>
+        )}
+        {flag ? (
+          <FlatList
+            bounces={false}
+            contentContainerStyle={{ marginTop: scale(5) }}
+            keyExtractor={(item) => item.index}
+            data={brandList}
+            renderItem={_renderItem}
+          />
+        ) : (
+          <FlatList
+            bounces={false}
+            contentContainerStyle={{ marginTop: scale(5) }}
+            keyExtractor={(item) => item.index}
+            data={recently}
+            renderItem={_renderItem1}
+          />
+        )}
       </SafeAreaView>
     </>
   );
@@ -159,5 +324,64 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     borderLeftColor: "transparent",
     borderRightColor: "transparent",
+  },
+  focustab: {
+    width: Width,
+    height: scale(44),
+    backgroundColor: "#f3f3f3",
+  },
+  focustabtext: {
+    fontFamily: "Roboto-Bold",
+    fontSize: scale(14),
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+  },
+  focustabsmallbox: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: scale(9),
+    borderRightWidth: scale(9),
+    borderTopWidth: scale(9),
+    backgroundColor: "transparent",
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+  },
+
+  brandimage: {
+    width: scale(65),
+    height: scale(40),
+  },
+  brandtext: {
+    fontFamily: "Roboto-Bold",
+    fontSize: scale(16),
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#222222",
+  },
+  count: {
+    fontFamily: "Roboto-Regular",
+    fontSize: scale(12),
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "right",
+    color: "#999999",
+  },
+  likes: {
+    width: scale(24),
+    height: scale(24),
+  },
+  delete: {
+    width: scale(15),
+    height: scale(15),
+  },
+  recentlytext: {
+    fontFamily: "Roboto-Bold",
+    fontSize: scale(14),
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#222222",
   },
 });
